2fd8f9c7e188a3d67f68191d0ff3984acf395e52
Phonemon <phonemon.off@gmail.com>
2017-06-27 20:55:51 +0200

    Update Monster.h

--- Phonemon/Monster/Monster.h
+++ Phonemon/Monster/Monster.h
@@ -28,5 +28,8 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+
+private:
+    UMonsterEntity *m_Monster;
 	
 };
