3d69998c971e6595ed9eb98514cbcf3334ebb19d
SinkThatBoat <uic21014@oepia.com>
2017-06-27 18:17:42 +0200

    Add new files

---
+++ Phonemon/Monster/MonsterEntity.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MonsterEntity.h"
+
+MonsterEntity::MonsterEntity()
+{
+}
+
+MonsterEntity::~MonsterEntity()
+{
+}
