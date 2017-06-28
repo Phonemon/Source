1d694bdbccd3be95b2fe642e44ad109b035edb6a
SinkThatBoat <uic21014@oepia.com>
2017-06-28 20:11:21 +0200

    Bug fixes

--- Phonemon/Environment/Teleporter.cpp
+++ Phonemon/Environment/Teleporter.cpp
@@ -6,7 +6,7 @@
 // Sets default values
 ATeleporter::ATeleporter() {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	// Create collision
 	UBoxComponent *Box = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollision"));
